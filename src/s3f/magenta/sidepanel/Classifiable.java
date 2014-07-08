f9f6558321dd80bd26b75fd243d347c9f20e52eb
Anderson Antunes <anderson.utf@gmail.com>
2014-07-08 01:46:39 -0300

    primeiro commit, tudo funcionando como deveria, mas falta simplificar muito as coisas...

---
+++ src/s3f/magenta/sidepanel/Classifiable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s3f.magenta.sidepanel;
+
+/**
+ *
+ * @author antunes
+ */
+public interface Classifiable {
+
+    public Item getItem();
+}
---
+++ src/s3f/magenta/sidepanel/Configurable.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package s3f.magenta.sidepanel;
+
+import s3f.magenta.GraphicObject;
+import s3f.magenta.swing.WidgetContainer;
+
+/**
+ *
+ * @author anderson
+ */
+public interface Configurable {
+
+    public WidgetContainer getConfigurationPanel();
+    
+}
